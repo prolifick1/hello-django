b19d29528ac01bd360686faa68926db4fcb3ef88
Luis Manzo <manzosk3@gmail.com>
2022-06-21 14:57:16 -0500

    complete, todo: verify that it works

---
+++ hello_django/hello_django/urls.py
@@ -0,0 +1,36 @@
+"""hello_django URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.http import HttpResponse
+import math
+
+def rectArea(request, height, width):
+    return HttpResponse(f'<h1>{height * width}<h1>')
+def rectPerimeter(request, height, width):
+    return HttpResponse(f'<h1>{2*height + 2*width}<h1>')
+def circArea(request, radius):
+    return HttpResponse(f'<h2>{round(math.pi*(radius)**2, 2)}</h2>')
+def circPerimeter(request, radius):
+    return HttpResponse(f'<h2>{round(math.pi*2*radius, 2)}')
+
+urlpatterns = [
+    path('rectangle/area/<int:height>/<int:width>', rectArea), 
+    path('rectangle/perimeter/<int:height>/<int:width>', rectPerimeter), 
+    path('circle/area/<int:radius>', circArea), 
+    path('circle/perimeter/<int:radius>', circPerimeter), 
+    path('admin/', admin.site.urls),
+]
